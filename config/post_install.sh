80b2617f5b6256165f00ed5be4af8ceb00f88e30
Gormak <nwierzbinski@gmail.com>
2019-02-22 18:40:00 +0100

    deleted:    .vagrant/machines/default/virtualbox/action_provision 	deleted:    .vagrant/machines/default/virtualbox/action_set_name 	deleted:    .vagrant/machines/default/virtualbox/box_meta 	deleted:    .vagrant/machines/default/virtualbox/creator_uid 	deleted:    .vagrant/machines/default/virtualbox/id 	deleted:    .vagrant/machines/default/virtualbox/index_uuid 	deleted:    .vagrant/machines/default/virtualbox/private_key 	deleted:    .vagrant/machines/default/virtualbox/synced_folders 	deleted:    .vagrant/machines/default/virtualbox/vagrant_cwd 	modified:   config/post_install.sh

--- config/post_install.sh
+++ config/post_install.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 echo "######################## MIGRATION #######################"
 cd /home/vagrant/www/
+composer update
 php artisan migrate
 php artisan db:seed
 php -S 192.168.55.55:8000 -t /home/vagrant/www/public

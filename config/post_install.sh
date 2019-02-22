21a72e94276b45b7baf53f0abbc9a84f8f4a4471
Gormak <nwierzbinski@gmail.com>
2019-02-22 19:16:44 +0100

    modified:   config/post_install.sh

--- config/post_install.sh
+++ config/post_install.sh
@@ -4,4 +4,4 @@ cd /home/vagrant/www/
 composer update
 php artisan migrate
 php artisan db:seed
-php -S 192.168.55.55:8000 -t /home/vagrant/www/public
+php -S 192.168.55.55:8000 -t /home/vagrant/www/public &
